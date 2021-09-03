af941a7eebbc148d83a896bafafacc276e789bcc
Raúl Cabezas González <raul.29cg@gmail.com>
2021-09-03 10:45:11 +0200

    Step 1

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 20:29:07 by rcabezas          #+#    #+#             */
-/*   Updated: 2021/08/31 12:44:46 by rcabezas         ###   ########.fr       */
+/*   Updated: 2021/09/03 10:33:57 by rcabezas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,6 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-typedef struct s_data
-{
-	int	no_philos;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	eating_times;
-	t_philo	*philos;
-}	t_data;
-
 typedef struct s_philo
 {
 	int	id;
@@ -36,17 +26,30 @@ typedef struct s_philo
 	int	time_to_eat;
 	int	time_to_sleep;
 	int	eating_times;
+	pthread_t	thread;
 }	t_philo;
 
+typedef struct s_data
+{
+	int	no_philos;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	eating_times;
+	t_philo	*philos;
+	pthread_t	*threads;
+}	t_data;
+
 //philo.c
-int	main(int argc, char **argv);
+int		main(int argc, char **argv);
 void	init_args(t_data *d, char **argv, int argc);
 void	create_philos(t_data *d);
+void	waiting_room(t_data *d);
 
 //ft.c
-int	ft_atoi(const char *str);
+int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
 
 //error.c
-void	handle_errors(int error);
+void	error_handling(int error);
 #endif
--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -6,23 +6,28 @@
 /*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 20:30:29 by rcabezas          #+#    #+#             */
-/*   Updated: 2021/08/31 13:46:45 by rcabezas         ###   ########.fr       */
+/*   Updated: 2021/09/03 10:36:31 by rcabezas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
 
-void	philo_do(t_data *d)
+void	waiting_room(t_data *d)
 {
-	
+	int	i;
+
+	i = 0;
+	while(i < d->no_philos)
+	{
+		pthread_join(d->philos[i].thread, NULL);
+		i++;
+	}
 }
 
-void	init_philos(t_data *d, int i)
+void	*philo_do(void *arg)
 {
-	pthread_t	thread;
-
-	d->philos[i - 1].id = i;
-	pthread_create(thread, NULL, philo_do, d);
+	printf("Soy el filosofo %i\n", *(int *)arg);
+	return (0);
 }
 
 void	init_args(t_data *d, char **argv, int argc)
@@ -39,11 +44,12 @@ void	create_philos(t_data *d)
 {
 	int i;
 	
-	i = 1;
+	i = 0;
 	d->philos = malloc(sizeof(t_philo) * d->no_philos);
-	while (i <= d->no_philos)
+	while (i < d->no_philos)
 	{
-		init_philos(d, i);
+		d->philos[i].id = i;
+		pthread_create(&d->philos[i].thread, NULL, philo_do, &d->philos[i].id);
 		i++;
 	}
 }
@@ -53,7 +59,7 @@ int	main(int argc, char **argv)
 	t_philo	*p;
 	t_data	*d;
 
-	if (argc != 5 || argc != 6)
+	if (argc != 5 && argc != 6)
 		error_handling(1);
 	p = malloc(sizeof(t_philo));
 	ft_bzero(p, sizeof(t_philo));
@@ -61,4 +67,5 @@ int	main(int argc, char **argv)
 	ft_bzero(d, sizeof(t_philo));
 	init_args(d, argv, argc);
 	create_philos(d);
+	waiting_room(d);
 }
