040b69f0dc09c153efa28f3f75b1b4766ad590b9
Raúl Cabezas González <raul.29cg@gmail.com>
2021-09-06 17:40:57 +0200

    Made forks and eating functions

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 20:29:07 by rcabezas          #+#    #+#             */
-/*   Updated: 2021/09/04 21:35:52 by rcabezas         ###   ########.fr       */
+/*   Updated: 2021/09/06 17:26:58 by rcabezas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <time.h>
+# include <sys/time.h>
 
 typedef struct s_philo
 {
@@ -29,8 +30,9 @@ typedef struct s_philo
 	int			eating_times;
 	pthread_t	thread;
 	int			eaten_times;
-	pthread_mutex_t left_fork;
-	pthread_mutex_t right_fork;
+	pthread_mutex_t *left_fork;
+	pthread_mutex_t *right_fork;
+	uint64_t	start_time;
 }	t_philo;
 
 typedef struct s_data
@@ -43,7 +45,7 @@ typedef struct s_data
 	t_philo		*philos;
 	pthread_t	*threads;
 	pthread_mutex_t	*forks;
-	time_t		start_time;
+	uint64_t	start_time;
 }	t_data;
 
 //philo.c
@@ -52,6 +54,7 @@ void	init_args(t_data *d, char **argv, int argc);
 void	create_philos(t_data *d);
 void	waiting_room(t_data *d);
 void	create_forks(t_data *d);
+void	philo_chars(t_data *d, int i);
 
 //ft.c
 int		ft_atoi(const char *str);
@@ -59,4 +62,14 @@ void	ft_bzero(void *s, size_t n);
 
 //error.c
 void	error_handling(int error);
+
+//actions.c
+void	philo_eat(t_philo *philo);
+void	philo_sleep(t_philo *philo);
+void	philo_think(t_philo *philo);
+
+//time.c
+uint64_t	get_timeval_ms(struct timeval *time);
+void		start_time(t_data *d);
+uint64_t	get_time(uint64_t start_time);
 #endif
--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 20:30:29 by rcabezas          #+#    #+#             */
-/*   Updated: 2021/09/04 21:46:31 by rcabezas         ###   ########.fr       */
+/*   Updated: 2021/09/06 17:36:34 by rcabezas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	create_forks(t_data *d)
 	d->forks = malloc(sizeof(pthread_mutex_t) * d->no_philos);
 	while (i < d->no_philos)
 	{
-		pthread_mutexattr_init(&d->forks[i]);
+		pthread_mutex_init(&d->forks[i], NULL);
 		i++;
 	}
 }
@@ -39,9 +39,12 @@ void	waiting_room(t_data *d)
 
 void	*philo_do(void *arg)
 {
-	philo_eat();
-	philo_sleep();
-	philo_think();
+	while (1)
+	{
+		philo_eat((t_philo *)arg);
+		philo_sleep((t_philo *)arg);
+		philo_think((t_philo *)arg);	
+	}
 	return (0);
 }
 
@@ -55,16 +58,32 @@ void	init_args(t_data *d, char **argv, int argc)
 		d->eating_times = ft_atoi(argv[5]);
 }
 
+void	philo_chars(t_data *d, int i)
+{
+	d->philos[i].start_time = d->start_time;
+	d->philos[i].id = i;
+	d->philos[i].time_to_eat = d->time_to_eat;
+	d->philos[i].time_to_die = d->time_to_die;
+	d->philos[i].time_to_sleep = d->time_to_sleep;
+	d->philos[i].eating_times = d->eating_times;
+	if (i == d->no_philos - 1)
+		d->philos[i].right_fork = &d->forks[0];
+	else
+		d->philos[i].right_fork = &d->forks[i + 1];
+	d->philos[i].left_fork = &d->forks[i];
+}
+
 void	create_philos(t_data *d)
 {
 	int i;
 	
 	i = 0;
 	d->philos = malloc(sizeof(t_philo) * d->no_philos);
+	start_time(d);
 	while (i < d->no_philos)
 	{
-		d->philos[i].id = i;
-		pthread_create(&d->philos[i].thread, NULL, philo_do, &d->philos[i].id);
+		philo_chars(d, i);
+		pthread_create(&d->philos[i].thread, NULL, philo_do, &d->philos[i]);
 		i++;
 	}
 }
