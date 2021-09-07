d1f6422710eac59ec35fd1dda4e51dc644a70e6e
rcabezas29 <rcabezas@c1r3s3.42madrid.com>
2021-09-07 21:28:19 +0200

    Erro at last_meal

--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 20:30:29 by rcabezas          #+#    #+#             */
-/*   Updated: 2021/09/07 17:53:35 by rcabezas         ###   ########.fr       */
+/*   Updated: 2021/09/07 21:24:05 by rcabezas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,11 @@ void	looking_for_die(t_data *d)
 		i = 0;
 		while (i < d->no_philos)
 		{
+			/////////////////////////////////
 			if ((uint64_t)d->philos[i].time_to_eat < get_time(get_timeval_ms(&d->philos[i].last_meal)))
 				philo_die(&d->philos[i]);
 			i++;
+			////////////////////////////////
 		}
 	}
 }
