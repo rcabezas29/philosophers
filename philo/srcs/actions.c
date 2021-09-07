bab2426bd03e85f363bca3c7f0a52f21be84957e
Raúl Cabezas González <raul.29cg@gmail.com>
2021-09-07 15:35:55 +0200

    Trying kill philos

--- philo/srcs/actions.c
+++ philo/srcs/actions.c
@@ -6,7 +6,7 @@
 /*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/04 21:43:16 by rcabezas          #+#    #+#             */
-/*   Updated: 2021/09/06 17:37:54 by rcabezas         ###   ########.fr       */
+/*   Updated: 2021/09/06 21:29:15 by rcabezas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,10 @@ void	philo_eat(t_philo *philo)
 {
 	pthread_mutex_lock(philo->right_fork);
 	pthread_mutex_lock(philo->left_fork);
-	printf("(\033[0;32m%llu\033[0;38m) - \033[0;34mEl filosofo %i esta comiendo \033[0;38m🍽\n", get_time(philo->start_time), philo->id);
+	printf("(\033[0;32m%llu\033[0;38m) - \033[0;33mEl filosofo (%i) ha cogido el tenedor derecho \033[0;38m 🍴\n", get_time(philo->start_time), philo->id);
+	printf("(\033[0;32m%llu\033[0;38m) - \033[0;33mEl filosofo (%i) ha cogido el tenedor izquierdo \033[0;38m 🍴\n", get_time(philo->start_time), philo->id);
+	printf("(\033[0;32m%llu\033[0;38m) - \033[0;34mEl filosofo (%i) esta comiendo \033[0;38m 🍝\n", get_time(philo->start_time), philo->id);
+	gettimeofday(&philo->last_meal, NULL);
 	usleep(philo->time_to_eat * 1000);
 	philo->eaten_times += 1;
 	pthread_mutex_unlock(philo->right_fork);
@@ -25,12 +28,11 @@ void	philo_eat(t_philo *philo)
 
 void	philo_sleep(t_philo *philo)
 {
-	//printf("El filosofo %i esta durmiendo\n", philo->id);
+	printf("(\033[0;32m%llu\033[0;38m) - \033[0;36mEl filosofo (%i) esta durmiendo \033[0;38m 💤\n", get_time(philo->start_time), philo->id);
 	usleep(philo->time_to_sleep * 1000);
 }
 
 void	philo_think(t_philo *philo)
 {
-	//printf("El filosofo %i esta pensando\n", philo->id);
-	usleep(philo->time_to_sleep * 1000);
+	printf("(\033[0;32m%llu\033[0;38m) - \033[0;35mEl filosofo (%i) esta pensando \033[0;38m 💭\n", get_time(philo->start_time), philo->id);
 }
