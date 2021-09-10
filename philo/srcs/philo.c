532679ad95ba69c9e8156732b62c8dd41a51a095
Raúl Cabezas González <raul.29cg@gmail.com>
2021-09-10 10:10:54 +0200

    Done eating times

--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 20:30:29 by rcabezas          #+#    #+#             */
-/*   Updated: 2021/09/09 21:06:46 by rcabezas         ###   ########.fr       */
+/*   Updated: 2021/09/10 10:10:25 by rcabezas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@ void	looking_for_die(t_data *d)
 			if ((uint64_t)d->philos[i].time_to_die
 				< get_time(get_timeval_ms(&d->philos[i].last_meal)))
 				philo_die(&d->philos[i]);
-			if (d->eating_times!= 0 && d->philos[i].eaten_times == d->eating_times)
-				pthread_detach(d->philos[i].thread);
+			if (d->eating_times != 0 && d->philos[i].eaten_times == d->eating_times)
+				d->philos[i].ate = 1;
 			i++;
 		}
 	}
@@ -48,7 +48,7 @@ void	*philo_do(void *arg)
 	t_philo	*philo;
 
 	philo = (t_philo *)arg;
-	while (*philo->died != 1)
+	while (*philo->died != 1 && philo->ate != 1)
 	{
 		philo_eat(philo);
 		philo_sleep(philo);
