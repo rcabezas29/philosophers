6ee847e825232c817ef5ced26f25802bc221351a
Raúl Cabezas González <raul.29cg@gmail.com>
2021-09-14 18:13:01 +0200

    Finished

--- philo/srcs/actions.c
+++ philo/srcs/actions.c
@@ -6,7 +6,7 @@
 /*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/04 21:43:16 by rcabezas          #+#    #+#             */
-/*   Updated: 2021/09/13 20:38:30 by rcabezas         ###   ########.fr       */
+/*   Updated: 2021/09/14 17:59:56 by rcabezas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	philo_eat(t_philo *philo)
 {
 	philo_take_fork(philo);
-	if (*philo->died != 1 && philo->ate != 1)
+	if (*philo->died != 1)
 		printf("[%llu] - (%i) is eating 🍝\n",
 			get_time(philo->start_time), philo->id);
 	gettimeofday(&philo->last_meal, NULL);
@@ -27,7 +27,7 @@ void	philo_eat(t_philo *philo)
 
 void	philo_sleep(t_philo *philo)
 {
-	if (*philo->died != 1 && philo->ate != 1)
+	if (*philo->died != 1)
 		printf("[%llu] - (%i) is sleeping 💤\n",
 			get_time(philo->start_time), philo->id);
 	ft_usleep(philo->time_to_sleep);
@@ -35,7 +35,7 @@ void	philo_sleep(t_philo *philo)
 
 void	philo_think(t_philo *philo)
 {
-	if (*philo->died != 1 && philo->ate != 1)
+	if (*philo->died != 1)
 		printf("[%llu] - (%i) is thinking 💭\n",
 			get_time(philo->start_time), philo->id);
 	usleep(10);
@@ -55,11 +55,11 @@ void	philo_take_fork(t_philo *philo)
 	if (philo->id % 2)
 	{
 		pthread_mutex_lock(philo->right_fork);
-		if (*philo->died != 1 && philo->ate != 1)
+		if (*philo->died != 1)
 			printf("[%llu] - (%i) has taken right fork  🍴\n",
 				get_time(philo->start_time), philo->id);
 		pthread_mutex_lock(philo->left_fork);
-		if (*philo->died != 1 && philo->ate != 1)
+		if (*philo->died != 1)
 			printf("[%llu] - (%i) has taken left fork  🍴\n",
 				get_time(philo->start_time), philo->id);
 	}
